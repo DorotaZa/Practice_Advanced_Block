d2a4e137e09f1560585ad70a600ec1021fb65daf
Dorota Zak <dorotaa.zak@gmail.com>
2022-08-30 16:02:51 +0000

    First commit: Zadanie_1 + Zadanie_2

---
+++ Zadanie_1/main.cpp
@@ -0,0 +1,25 @@
+﻿// Zadanie_1
+
+//Napisz funkcję, która odwróci podanego stringa.
+
+
+#include <iostream>
+#include <string>
+
+void reverseString (std::string str)
+{
+    for (int i = str.length() - 1; i >= 0; --i)
+    {
+        std::cout << str.at(i);
+    }
+}
+
+
+int main()
+{
+
+    std::cout << "Hello World!" << std::endl;
+    std::string str = "Hello World!";
+    reverseString(str);
+
+}
---
+++ Zadanie_2/Zadanie_2.cpp
@@ -0,0 +1,36 @@
+﻿// Zadanie_2
+
+//Napisz funkcję, która przyjmie dwa stringi i zwróci ile liter mają wspólnych.
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector <char> mutualLetters (std::string first, std::string second)
+{
+    std::vector <char> vec;
+    for (auto i = 0; i < first.length(); ++i)
+    {
+        if (first.at(i) == second.at(i))
+
+        {
+            vec.push_back(first.at(i));
+        }
+    }
+    return vec;
+}
+
+int main()
+{
+    std::string a = "Barbara";
+    std::string b = "Rabarbar";
+    std::vector<char> vec = mutualLetters(a, b);
+
+    auto print = mutualLetters(a, b);
+
+    for (const auto p : print)
+    {
+        std::cout << p;
+    }
+   
+}
